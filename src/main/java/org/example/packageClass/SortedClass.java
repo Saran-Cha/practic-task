d81877c6177b18fea6200ecb0e44ac5767aee8db
Saran-Cha <musatovandrej656@gmail.com>
2026-03-11 15:29:51 +0300

    в сортировку добавил доп.№1 задание

--- src/main/java/org/example/packageClass/SortedClass.java
+++ src/main/java/org/example/packageClass/SortedClass.java
@@ -3,6 +3,7 @@ package org.example.packageClass;
 import org.example.model.Student;
 import org.example.packageInterface.SortProcessor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SortedClass implements SortProcessor {
@@ -11,33 +12,40 @@ public class SortedClass implements SortProcessor {
     public void sortedName(List<Student> student) {
 
         System.out.println();
-        // первый этап сортирует по первым буквам
-        for (int i = 0; i < student.size() - 1; i++) {
-            for (int j = 0; j < student.size() - i - 1; j++) {
-                if (student.get(j).getName().charAt(0) > student.get(j + 1).getName().charAt(0)) {
-                    // swap (перестановка)
-                    Student temp = student.get(j);
-                    student.set(j, student.get(j + 1));
-                    student.set(j + 1, temp);
-                }
-            }
-        }
-        // второй этап сортирует имена от второго символа и далее
-        for (int i = 0; i < student.size() - 1; i++) {
-            for (int j = 0; j < student.size() - i - 1; j++) {
-                int lim = Math.min(student.get(j).getName().length(), student.get(j+1).getName().length());
+
+
+        // Сортировка по алфавиту
+        boolean is_Sorted = false;
+        while (!is_Sorted) {
+            is_Sorted = true;
+            for (int i = 0; i < student.size() - 1; i++) {
+
+                String name1 = student.get(i).getName();
+                String name2 = student.get(i + 1).getName();
+                int lim = Math.min(name1.length(), name2.length());
                 int k = 0;
-                    // // Сравнение параметра name
-                while (k < lim - 1) {
-                    if (student.get(j).getName().charAt(k) == student.get(j + 1).getName().charAt(k) &&
-                            student.get(j).getName().charAt(k + 1) > student.get(j + 1).getName().charAt(k + 1)) {
+
+                while (k < lim) {
+                    if (name1.charAt(k) != name2.charAt(k)) {
+
                         // swap (перестановка)
-                        Student temp = student.get(j);
-                        student.set(j, student.get(j + 1));
-                        student.set(j + 1, temp);
-                        k = lim;
+                        if (name1.charAt(k) > name2.charAt(k)) {
+                            Student temp = student.get(i);
+                            student.set(i, student.get(i + 1));
+                            student.set(i + 1, temp);
+                            is_Sorted = false;
+                        }
+                        break;
                     }
                     k++;
+                }
+
+                // Сортировка по длине имени
+                if (k == lim && name1.length() > name2.length()){
+                    Student temp = student.get(i);
+                    student.set(i, student.get(i + 1));
+                    student.set(i + 1, temp);
+                    is_Sorted = false;
                 }
 
             }
@@ -51,44 +59,121 @@ public class SortedClass implements SortProcessor {
 
     @Override
     public void sortedGrade(List<Student> student) {
-
         System.out.println();
-        // далее блок сортировки по оценочному баллу
-        for (int i = 0; i < student.size() - 1; i++) {
-            for (int j = 0; j < student.size() - i - 1; j++) {
-                // Сравнение параметра grade
-                if (student.get(j).getGrade() < student.get(j + 1).getGrade()) {
-                    // swap (перестановка)
-                    Student temp = student.get(j);
-                    student.set(j, student.get(j + 1));
-                    student.set(j + 1, temp);
+
+        //Студенты все
+        List<Student> Student_list1 = new ArrayList<>(student);
+        //Сортировка
+        Sort_Grade(Student_list1);
+        Student_list1.forEach(System.out::println);
+        System.out.println("\nДанные отсортированы по оценочному баллу\n");
+
+
+        //Студенты с чётными значениями
+        List<Student> Student_list2 = new ArrayList<>(student);
+        //Студенты с чётными значениями
+        List<Student> evenStudents = new ArrayList<>();
+        for (Student s : student) {
+            if (s.getGrade() % 2 == 0) {
+                evenStudents.add(s);
+            }
+        }
+
+        //Сортировка только чётных значений
+        Sort_Grade(evenStudents);
+
+        //Замена чётных значений в основном списке на отсортированные чётные значения
+        int evenIndex = 0;
+        for (int i = 0; i < student.size(); i++) {
+            if (student.get(i).getGrade() % 2 == 0) {
+                student.set(i, evenStudents.get(evenIndex));
+                evenIndex++;
+            }
+        }
+
+        Student_list2.forEach(System.out::println);
+        System.out.println("\nДанные отсортированы по дополнительному заданию №1");
+    }
+
+    // Метод по сортировки оценок
+    private void Sort_Grade(List<Student> list) {
+        int n = list.size();
+        // блок сортировки по оценке
+        boolean is_Sorted = false;
+        while (!is_Sorted){
+            is_Sorted =true;
+            for (int i = 0; i < n - 1; i++) {
+                // Сравнение параметра Grade
+                if (list.get(i).getGrade() < list.get(i + 1).getGrade()) {
+                    // swap
+                    Student temp = list.get(i);
+                    list.set(i, list.get(i + 1));
+                    list.set(i + 1, temp);
+                    is_Sorted = false;
                 }
             }
         }
-        student.forEach(System.out::println);
-        System.out.println("\nДанные отсортированы по оценочному баллу");
-
     }
 
     @Override
     public void sortedGradebookNumber(List<Student> student) {
 
         System.out.println();
-        // далее блок сортировки по номеру зачетной книжки
-        for (int i = 0; i < student.size() - 1; i++) {
-            for (int j = 0; j < student.size() - i - 1; j++) {
-                // Сравнение параметра gradebookNumberStudent
-                if (student.get(j).getGradebookNumber() > student.get(j + 1).getGradebookNumber()) {
-                    // swap (перестановка)
-                    Student temp = student.get(j);
-                    student.set(j, student.get(j + 1));
-                    student.set(j + 1, temp);
+        System.out.println();
+
+        //Студенты все
+        List<Student> Student_list1 = new ArrayList<>(student);
+        //Сортировка
+        Sort_GradebookNumber(Student_list1);
+        Student_list1.forEach(System.out::println);
+        System.out.println("\nДанные отсортированы по номеру студенческого билета\n");
+
+
+        //Студенты с чётными значениями
+        List<Student> Student_list2 = new ArrayList<>(student);
+        //Студенты с чётными значениями
+        List<Student> evenStudents = new ArrayList<>();
+        for (Student s : student) {
+            if (s.getGrade() % 2 == 0) {
+                evenStudents.add(s);
+            }
+        }
+
+        //Сортировка только чётных значений
+        Sort_GradebookNumber(evenStudents);
+
+        //Замена чётных значений в основном списке на отсортированные чётные значения
+        int evenIndex = 0;
+        for (int i = 0; i < student.size(); i++) {
+            if (student.get(i).getGrade() % 2 == 0) {
+                student.set(i, evenStudents.get(evenIndex));
+                evenIndex++;
+            }
+        }
+
+        Student_list2.forEach(System.out::println);
+        System.out.println("\nДанные отсортированы по дополнительному заданию №1");
+
+    }
+
+    // Метод по сортировки студаков
+    private void Sort_GradebookNumber(List<Student> list) {
+        int n = list.size();
+        // блок сортировки по оценке
+        boolean is_Sorted = false;
+        while (!is_Sorted){
+            is_Sorted =true;
+            for (int i = 0; i < n - 1; i++) {
+                // Сравнение параметра Grade
+                if (list.get(i).getGrade() < list.get(i + 1).getGrade()) {
+                    // swap
+                    Student temp = list.get(i);
+                    list.set(i, list.get(i + 1));
+                    list.set(i + 1, temp);
+                    is_Sorted = false;
                 }
             }
         }
-        student.forEach(System.out::println);
-        System.out.println("\nДанные отсортированы по номеру зачетной книжки");
-
     }
 
 }
